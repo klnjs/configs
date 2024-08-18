f036b3edc124a28b7b7ecf6315c6ddd2cbfcc70d
Rune Klein <runeklein@outlook.com>
2024-08-18 22:21:54 +0200

    🐛 asd

--- packages/configs/eslint-config/test/specs/core.spec.js
+++ packages/configs/eslint-config/test/specs/core.spec.js
@@ -8,23 +8,23 @@ import {
 } from '../helpers/eslint.js'
 import config from '../../src/core.js'
 
-const availableRules = getAvailableCoreRules()
-const configuredRules = getConfiguredCoreRules(config)
+const availableCoreRules = getAvailableCoreRules()
+const configuredCoreRules = getConfiguredCoreRules(config)
 
 test('Config should load', () => {
 	expect(() => createESLint(config).lintText('')).not.toThrow()
 })
 
 test('Config should include required rules', () =>
-	availableRules.forEach((rule, name) => {
+	availableCoreRules.forEach((rule, name) => {
 		if (!isDeprecatedRule(rule) && !isLayoutRule(rule)) {
-			expect(configuredRules).toHaveEntry(name)
+			expect(configuredCoreRules).toHaveEntry(name)
 		}
 	}))
 
 test('Config should exclude layout and deprecated rules', () =>
-	configuredRules.forEach((_value, name) => {
-		expect(availableRules).toHaveEntry(name)
-		expect(availableRules.get(name)).not.toBeLayoutRule(name)
-		expect(availableRules.get(name)).not.toBeDeprecatedRule(name)
+	configuredCoreRules.forEach((_value, name) => {
+		expect(availableCoreRules).toHaveEntry(name)
+		expect(availableCoreRules.get(name)).not.toBeLayoutRule(name)
+		expect(availableCoreRules.get(name)).not.toBeDeprecatedRule(name)
 	}))
