617012587760230b3649b39362c67082631f1f30
Rune Klein <runeklein@outlook.com>
2024-09-18 10:42:09 +0200

    ✨ Add jsx-a11y to eslint react config

--- packages/configs/eslint-config/src/react.js
+++ packages/configs/eslint-config/src/react.js
@@ -1,6 +1,7 @@
 import reactPlugin from 'eslint-plugin-react'
 import reactHooksPlugin from 'eslint-plugin-react-hooks'
 import reactCompilerPlugin from 'eslint-plugin-react-compiler'
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y'
 
 export default {
 	name: '@klnjs/react',
@@ -8,7 +9,8 @@ export default {
 	plugins: {
 		react: reactPlugin,
 		'react-hooks': reactHooksPlugin,
-		'react-compiler': reactCompilerPlugin
+		'react-compiler': reactCompilerPlugin,
+		'jsx-a11y': jsxA11yPlugin 
 	},
 	settings: {
 		react: {
@@ -143,6 +145,42 @@ export default {
 		'react/void-dom-elements-no-children': 'error',
 		'react-hooks/exhaustive-deps': 'error',
 		'react-hooks/rules-of-hooks': 'error',
-		'react-compiler/react-compiler': 'error'
+		'react-compiler/react-compiler': 'error',
+		'jsx-a11y/alt-text': 'error',
+		'jsx-a11y/anchor-ambiguous-text': 'off',
+		'jsx-a11y/anchor-has-content': 'error',
+		'jsx-a11y/anchor-is-valid': 'error',
+		'jsx-a11y/aria-activedescendant-has-tabindex': 'error',
+		'jsx-a11y/aria-props': 'error',
+		'jsx-a11y/aria-proptypes': 'error',
+		'jsx-a11y/aria-role': 'error',
+		'jsx-a11y/aria-unsupported-elements': 'error',
+		'jsx-a11y/autocomplete-valid': 'error',
+		'jsx-a11y/click-events-have-key-events': 'off',
+		'jsx-a11y/control-has-associated-label': 'off',
+		'jsx-a11y/heading-has-content': 'error',
+		'jsx-a11y/html-has-lang': 'error',
+		'jsx-a11y/iframe-has-title': 'error',
+		'jsx-a11y/img-redundant-alt': 'error',
+		'jsx-a11y/interactive-supports-focus': 'error',
+		'jsx-a11y/label-has-associated-control': 'error',
+		'jsx-a11y/lang': 'off',
+		'jsx-a11y/media-has-caption': 'error',
+		'jsx-a11y/mouse-events-have-key-events': 'error',
+		'jsx-a11y/no-access-key': 'error',
+		'jsx-a11y/no-aria-hidden-on-focusable': 'off',
+		'jsx-a11y/no-autofocus': 'error',
+		'jsx-a11y/no-distracting-elements': 'error',
+		'jsx-a11y/no-interactive-element-to-noninteractive-role': 'error',
+		'jsx-a11y/no-noninteractive-element-interactions': 'error',
+		'jsx-a11y/no-noninteractive-element-to-interactive-role': 'error',
+		'jsx-a11y/no-noninteractive-tabindex': 'error',
+		'jsx-a11y/no-redundant-roles': 'error',
+		'jsx-a11y/no-static-element-interactions': 'error',
+		'jsx-a11y/prefer-tag-over-role': 'off',
+		'jsx-a11y/role-has-required-aria-props': 'error',
+		'jsx-a11y/role-supports-aria-props': 'error',
+		'jsx-a11y/scope': 'error',
+		'jsx-a11y/tabindex-no-positive': 'error',
 	}
 }
