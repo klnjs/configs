6380fd99759ed577671bc24fd16030787ac358e5
Rune Klein <runeklein@outlook.com>
2024-12-15 19:50:57 +0100

    fix svelte prettier config

--- packages/configs/prettier-config/src/svelte.js
+++ packages/configs/prettier-config/src/svelte.js
@@ -1,7 +1,7 @@
 export default {
 	plugins: ['prettier-plugin-svelte'],
+	svelte5CompilerPath: '', // This is not mentioned in docs...
 	svelteSortOrder: 'options-scripts-markup-styles',
-	svelteStrictMode: true,
 	svelteAllowShorthand: true,
 	svelteIndentScriptAndStyle: true
 }
